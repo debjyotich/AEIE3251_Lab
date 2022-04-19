f708dfc6cd87829c09339438fc2dff8cf256e5f9
debjyoti <debjyoti.chowdhury@heritageit.edu>
2022-04-19 10:28:00 +0530

    code form 19-04 added

--- iot_server.py
+++ iot_server.py
@@ -1,5 +1,6 @@
 # Experiment no. 3a
 from flask import Flask, render_template
+from flask_restful import reqparse
 
 app = Flask(__name__)
 
@@ -10,5 +11,22 @@ def home():
     return render_template("index.html")
 
 
+# Experiment no. 3c
+@app.route("/update", methods=["GET"])
+def update():
+    parser = reqparse.RequestParser()
+
+    parser.add_argument("key", type=str)
+    parser.add_argument("sensor_data", type=float)
+
+    data_iot_board = parser.parse_args()
+
+    if data_iot_board["key"] == "AABB":
+        print(data_iot_board)
+        return "200 OK"
+    else:
+        return "404"
+
+
 if __name__ == "__main__":
-    app.run(host="192.168.26.13")
+    app.run(host="192.168.26.14")

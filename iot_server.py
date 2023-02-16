fb833b9891834eea2908a9acbd07cc9def43700a
debjyotich <86590192+debjyotich@users.noreply.github.com>
2023-02-16 09:25:59 +0530

    Update iot_server.py

--- iot_server.py
+++ iot_server.py
@@ -1,6 +1,7 @@
 # Experiment no. 3a
 from flask import Flask, render_template
 from flask_restful import reqparse
+import datetime
 
 app = Flask(__name__)
 
@@ -16,17 +17,29 @@ def home():
 def update():
     parser = reqparse.RequestParser()
 
-    parser.add_argument("key", type=str)
+    parser.add_argument("api_key", type=str)
     parser.add_argument("sensor_data", type=float)
 
-    data_iot_board = parser.parse_args()
+    data = parser.parse_args()
+    data['date'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(data)
 
-    if data_iot_board["key"] == "AABB":
-        print(data_iot_board)
-        return "200 OK"
-    else:
-        return "404"
+    return "200"
+
+
+@app.route("/feed", methods=["GET"])
+def feed():
+    parser = reqparse.RequestParser()
+
+    parser.add_argument("api_key", type=str)
+
+    data = parser.parse_args()
+
+
+    print(data)
+
+    return "200"
 
 
 if __name__ == "__main__":
-    app.run(host="192.168.26.14")
+    app.run(host="192.168.26.15")
